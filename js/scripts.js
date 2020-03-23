54c6349d100bf6334f2e56be732007e7c5e3509b
Julia Seidman <juliaseid@gmail.com>
2020-03-23 16:35:53 -0700

    Add jquery functions

---
+++ js/scripts.js
@@ -0,0 +1,15 @@
+$(document).ready(function(){
+  
+  $("h1").click(function(){
+    alert("This is header.")
+  });
+  
+  $("p").click(function(){
+    alert("This is paragraph.");
+  });
+  
+  $("img").click(function(){
+    alert("This is an image.");
+  });
+
+});
