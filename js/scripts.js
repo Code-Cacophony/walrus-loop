8d28bb5361c666b8dd9f15d0105d821a37c93321
Julia Seidman <juliaseid@gmail.com>
2020-03-30 14:11:57 -0700

    Try to get looping working on scripts

--- js/scripts.js
+++ js/scripts.js
@@ -1,15 +1,35 @@
 $(document).ready(function(){
   
-  $("h1").click(function(){
-    alert("This is header.")
-  });
-  
-  $("p").click(function(){
-    alert("This is paragraph.");
-  });
-  
-  $("img").click(function(){
-    alert("This is an image.");
+//      $("h1").click(function(){
+//        alert("This is header.")
+//      });
+      
+//      $("p").click(function(){
+//        alert("This is paragraph.");
+//      });
+      
+//      $("img").click(function(){
+//        alert("This is an image.");
+//      });
+
+
+
+  var types = ["h1", "p", "img"]
+  types.forEach(function(type) {
+    $(type).click(function(type) {
+      if (type === "h1") {
+      alert("This is a header")
+    }
+    else if (type === "p") {
+      alert("This is a paragraph")
+    }
+    else if (type === "img") {
+      alert("This is an image")
+    }
   });
 
 });
+
+
+
+})
